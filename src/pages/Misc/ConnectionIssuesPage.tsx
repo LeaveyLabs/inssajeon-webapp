a7dc4b3f981c74fbf4ac6709938f681afbc087a7
adamvnovak <adamvnovak@protonmail.com>
2021-12-30 08:14:31 -0600

    fixed post bottom corners to not rounded

---
+++ src/pages/Misc/ConnectionIssuesPage.tsx
@@ -0,0 +1,27 @@
+// @mui
+import { Container, Typography } from '@mui/material';
+// hooks
+
+// components
+import Page from '../../components/Page';
+
+// ----------------------------------------------------------------------
+
+export default function ConnectionIssuesPage() {
+
+  return (
+    <Page title="홈">
+      <Container maxWidth={'fullscreen'}>
+        <Typography variant="h3" component="h1" paragraph>
+          word
+        </Typography>
+        <Typography gutterBottom>
+          description
+        </Typography>
+        <Typography>
+          quote
+        </Typography>
+      </Container>
+    </Page>
+  );
+}
