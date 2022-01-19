3c9a2a2291fe90587d3f0975ed8e525837f40d25
Adam Novak <adamnova@usc.edu>
2022-01-18 20:55:10 -0800

    Revert "Frontend spring2"

--- src/components/profile/ProfileFeed.tsx
+++ src/components/profile/ProfileFeed.tsx
@@ -1,10 +1,11 @@
 import { Divider, styled, ToggleButton, ToggleButtonGroup, useTheme } from '@mui/material';
 import React, { useState } from 'react';
 import Feed from 'src/components/feed/Feed';
+import MyProfileCard from 'src/components/profile/MyProfileCard';
 import { DataQuery, PostInteractionType, PostOrder } from 'src/db/apis/DataQuery';
 import { UserEntity } from 'src/db/entities/users/UserEntity';
 import useAuth from 'src/hooks/useAuth';
-import { useUpdateEffect } from 'src/hooks/useUpdateEffect';
+import { convertToObject } from 'typescript';
 import ProfileCard from './ProfileCard';
 
 interface ProfileCardProps {
@@ -30,7 +31,21 @@ const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
 
 
 export default function ProfileFeed( { profileUser }: ProfileCardProps ) {
+  const {authedUser} = useAuth();
+  const theme = useTheme();
   const [feedSelection, setFeedSelection] = useState<FeedSelection>(FeedSelection.submissions)
+
+  async function getNewPosts() {
+    try {
+      if (feedSelection === FeedSelection.submissions) {
+        return await DataQuery.searchPostByUserID(profileUser.id, PostInteractionType.Submission, PostOrder.Trendscore);
+      } else {
+        return await DataQuery.searchPostByUserID(profileUser.id, PostInteractionType.Favorite, PostOrder.Trendscore);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const handleFeedSelectionToggle = (event: React.MouseEvent<HTMLElement>, newFeedSelection: FeedSelection | null) => {
     if (newFeedSelection !== null) {
@@ -38,30 +53,20 @@ export default function ProfileFeed( { profileUser }: ProfileCardProps ) {
     }
   };
 
-  const queryGenerator = (feedSelection: FeedSelection) => async function getNewPosts(lastPage: any) {
-    try {
-      console.log("NEW FEED SELECTION: " + feedSelection)
-      return await DataQuery.searchPostByUserID(profileUser.id, PostInteractionType.Submission, PostOrder.Trendscore, lastPage);
-    } catch (error) {
-      console.log(error)
-    }
-  };
-  let [feedSelectionQueryCall, setFeedSelectionQueryCall] = useState(() => queryGenerator(feedSelection));
-  
-  useUpdateEffect(() => {
-    setFeedSelectionQueryCall(() => queryGenerator(feedSelection));
-  }, [feedSelection]);
-
   return (
     <> 
-      <ProfileCard profileUser={profileUser} />
+      {authedUser && authedUser.nonauth.profile.username === profileUser.profile.username ?
+        <ProfileCard profileUser={profileUser} /> //TODO consider changing back to myprofilecard
+      : 
+        <ProfileCard profileUser={profileUser} />
+      }
       <Divider variant="fullWidth" />
       <StyledToggleButtonGroup fullWidth value={feedSelection} exclusive onChange={handleFeedSelectionToggle}>
         <ToggleButton disableRipple value={FeedSelection.submissions}>게시물</ToggleButton>
         <ToggleButton disableRipple value={FeedSelection.favorites}>저장됨</ToggleButton>
       </StyledToggleButtonGroup>
       {/* <Divider sx={{mb:theme.spacing(2)}} variant="fullWidth" /> */}
-      <Feed getNewPosts={feedSelectionQueryCall} />
+      <Feed getNewPosts={getNewPosts} />
     </>
   )
 }
